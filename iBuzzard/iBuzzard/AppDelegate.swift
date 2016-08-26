fe9ad3826ee5c4e4e5ddd456e49bbcf61161c309
Krzysztof Profic <kprofic@gmail.com>
2016-08-26 14:56:17 +0200

    Playing buzzard sound on receiving push

--- iBuzzard/iBuzzard/AppDelegate.swift
+++ iBuzzard/iBuzzard/AppDelegate.swift
@@ -35,7 +35,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let tgr = UITapGestureRecognizer(target: self, action: #selector(AppDelegate.playStream))
         window?.addGestureRecognizer(tgr)
         
-        playStream()
         return true
     }
     
